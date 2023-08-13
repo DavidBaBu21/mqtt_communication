7522c6b40d7265646e6dbd84d28b3ae9643a3998
David Bailón Bustos <davidbailonbustos@outlook.com>
2023-08-13 12:19:43 -0600

    Update PublicadorEsp32.ino

--- PublicadorEsp32/PublicadorEsp32.ino
+++ PublicadorEsp32/PublicadorEsp32.ino
@@ -4,7 +4,7 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 const char* ssid = "Totalplay-2.4G-44a8";
-const char* password = "tntZbtg8pbKDqfqz";
+const char* password = "";
 const char* mqtt_server = "192.168.100.16";
 const char* mqtt_server2 = "127.0.0.1";
 
